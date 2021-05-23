e6d84372c67b38d11480e8714fd35c1399531267
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2021-05-23 23:29:50 +0700

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,3 @@
 <template>
-  <div class="navbar is-light">
-    <div class="navbar-brand">
-      <div class="navbar-item">
-        <router-link to="/">Home</router-link>
-      </div>
-      <div class="navbar-item">
-        <router-link to="/setting">Setting</router-link>
-      </div>
-    </div>
-  </div>
   <router-view class="container" />
 </template>
